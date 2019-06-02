6f9f64847b452e90a5cc85012bcb080e13effb49
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-06-02 15:34:35 +0900

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+/** @type {Parameters<typeof webpack>[0][0]} */
+const config = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: "bundle.[contenthash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, { loader: "css-loader" }]
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./webfonts",
+              publicPath: "../webfonts"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./images",
+              publicPath: "../images"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "css/[name].bundle.css" }),
+    new HtmlWebpackPlugin({
+      title: "ECCジュニア",
+      template: "src/index.html"
+    })
+  ]
+};
+
+module.exports = config;
